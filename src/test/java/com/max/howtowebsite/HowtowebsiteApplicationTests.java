27a68009ed8836ef46529af9a90ccb7e82fd36ab
maxkorzan <maxkorzan@gmail.com>
2020-02-11 11:46:52 -0500

    first commit

---
+++ src/test/java/com/max/howtowebsite/HowtowebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.max.howtowebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HowtowebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
